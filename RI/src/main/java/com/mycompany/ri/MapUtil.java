d22245b382a72770677131f19958cde17be3af46
abdel42 <abdel.madjid.brahimi@etu.univ-st-etienne.fr>
2019-11-03 19:13:52 +0100

    corrections des noms des runs et de la numérotation de

---
+++ RI/src/main/java/com/mycompany/ri/MapUtil.java
@@ -0,0 +1,38 @@
+package com.mycompany.ri;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MapUtil {
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
+    }
+
+
+
+
+
+    public static Iterator valueIterator(TreeMap map) {
+        Set set = new TreeSet(new Comparator<Map.Entry<String, Double>>() {
+            @Override
+            public int compare(Map.Entry<String, Double> o1, Map.Entry<String, Double> o2) {
+                return  o1.getValue().compareTo(o2.getValue()) > 0 ? 1 : -1;
+            }
+        });
+        set.addAll(map.entrySet());
+        return set.iterator();
+    }
+
+
+
+
+}
