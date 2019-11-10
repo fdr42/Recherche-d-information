937be1af2f5367e475d7801a2df7f1b194884f74
abdel42 <abdel.madjid.brahimi@etu.univ-st-etienne.fr>
2019-11-10 17:12:06 +0100

    Diverses corrections : nom, tri, taille runs Ajout de bugs et de code incompréhensible

--- RI/src/main/java/com/mycompany/ri/Indexation.java
+++ RI/src/main/java/com/mycompany/ri/Indexation.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 
 /**
  *
- * @author fdr
+ * @author fdr + madjid
  */
 public class Indexation {
 
@@ -91,6 +91,14 @@ public class Indexation {
 
         Map<String, Double> mapFinale = new HashMap<String, Double>();
 
+        //map pour chaque requete
+        Map<String, Double> mapQuery1 = new HashMap<String, Double>();
+        Map<String, Double> mapQuery2 = new HashMap<String, Double>();
+        Map<String, Double> mapQuery3 = new HashMap<String, Double>();
+        Map<String, Double> mapQuery4 = new HashMap<String, Double>();
+        Map<String, Double> mapQuery5 = new HashMap<String, Double>();
+        Map<String, Double> mapQuery6 = new HashMap<String, Double>();
+        Map<String, Double> mapQuery7 = new HashMap<String, Double>();
 
         String tag = "<>";
         int compteur = 0;
@@ -104,14 +112,380 @@ public class Indexation {
                 if (cosSim != 0.0) {
                     compteurNonNul++;
                 }
-                mapFinale.put(listeNumQ.get(q) + " Q0 " + listeDoc.get(compteurDoc).id + " " + tag + " " + cosSim + " TEAM /article[1]", cosSim);
-                System.out.println(listeNumQ.get(q) + " Q0 " + listeDoc.get(compteurDoc).id + " " + tag + " " + cosSim + " TEAM /article[1]");
+                mapFinale.put(listeNumQ.get(q) + " Q0 " + listeDoc.get(compteurDoc).id + " " + tag + " " + cosSim + " MadjidPierreBunyaminFrançois /article[1]", cosSim);
+                System.out.println(listeNumQ.get(q) + " Q0 " + listeDoc.get(compteurDoc).id + " " + tag + " " + cosSim + " MadjidPierreBunyaminFrançois /article[1]");
+                switch (q) {
+                    case 0:
+                        mapQuery1.put(listeNumQ.get(q) + " Q0 " + listeDoc.get(compteurDoc).id + " " + tag + " " + cosSim + " MadjidPierreBunyaminFrançois /article[1]", cosSim);
+                        break;
+                    case 1:
+                        mapQuery2.put(listeNumQ.get(q) + " Q0 " + listeDoc.get(compteurDoc).id + " " + tag + " " + cosSim + " MadjidPierreBunyaminFrançois /article[1]", cosSim);
+                        break;
+                    case 2:
+                        mapQuery3.put(listeNumQ.get(q) + " Q0 " + listeDoc.get(compteurDoc).id + " " + tag + " " + cosSim + " MadjidPierreBunyaminFrançois /article[1]", cosSim);
+                        break;
+                    case 3:
+                        mapQuery4.put(listeNumQ.get(q) + " Q0 " + listeDoc.get(compteurDoc).id + " " + tag + " " + cosSim + " MadjidPierreBunyaminFrançois /article[1]", cosSim);
+                        break;
+                    case 4:
+                        mapQuery5.put(listeNumQ.get(q) + " Q0 " + listeDoc.get(compteurDoc).id + " " + tag + " " + cosSim + " MadjidPierreBunyaminFrançois /article[1]", cosSim);
+                        break;
+                    case 5:
+                        mapQuery6.put(listeNumQ.get(q) + " Q0 " + listeDoc.get(compteurDoc).id + " " + tag + " " + cosSim + " MadjidPierreBunyaminFrançois /article[1]", cosSim);
+                        break;
+                    case 6:
+                        mapQuery7.put(listeNumQ.get(q) + " Q0 " + listeDoc.get(compteurDoc).id + " " + tag + " " + cosSim + " MadjidPierreBunyaminFrançois /article[1]", cosSim);
+                        break;
+                }
+
             }
+
+            /*
+
+
+            Mettre dans le runs par requete ici
+            Trier par score
+             */
         }
 
         Map<String, Double> mapTriee = new HashMap<String, Double>();
+        Map<String, Double> mapTriee1 = new HashMap<String, Double>();
+        Map<String, Double> mapTriee2 = new HashMap<String, Double>();
+        Map<String, Double> mapTriee3 = new HashMap<String, Double>();
+        Map<String, Double> mapTriee4 = new HashMap<String, Double>();
+        Map<String, Double> mapTriee5 = new HashMap<String, Double>();
+        Map<String, Double> mapTriee6 = new HashMap<String, Double>();
+        Map<String, Double> mapTriee7 = new HashMap<String, Double>();
 
-        mapTriee =  MapUtil.sortByValue(mapFinale);
+
+        /*
+
+        System.out.println("===============================================");
+        System.out.println("Taille mapQuery1 1 : "+mapQuery1.size());
+        System.out.println("Taille mapQuery1 2 : "+mapQuery2.size());
+        System.out.println("Taille mapQuery1 3 : "+mapQuery3.size());
+        System.out.println("Taille mapQuery1 4 : "+mapQuery4.size());
+        System.out.println("Taille mapQuery1 5 : "+mapQuery5.size());
+        System.out.println("Taille mapQuery1 6 : "+mapQuery6.size());
+        System.out.println("Taille mapQuery1 7 : "+mapQuery7.size());
+
+*/
+        mapTriee = MapUtil.sortByValue(mapFinale);
+
+
+        mapTriee1 =  MapUtil.sortByValue(mapQuery1);
+        mapTriee2 =  MapUtil.sortByValue(mapQuery2);
+        mapTriee3 =  MapUtil.sortByValue(mapQuery3);
+        mapTriee4 =  MapUtil.sortByValue(mapQuery4);
+        mapTriee5 =  MapUtil.sortByValue(mapQuery5);
+        mapTriee6 =  MapUtil.sortByValue(mapQuery6);
+        mapTriee7 =  MapUtil.sortByValue(mapQuery7);
+
+
+        /*
+
+
+
+        int limit = 0;
+        for (Map.Entry<String, Double> entry : mapQuery1.entrySet()) {
+            if(limit<100){
+                System.out.println(">>> "+entry.getKey());
+                limit++;
+            }
+        }
+        limit=0;
+        System.out.println("----------------");
+        for (Map.Entry<String, Double> entry : mapTriee1.entrySet()) {
+            if(limit<100){
+                System.out.println(">>> "+entry.getKey());
+                limit++;
+            }
+        }
+
+*/
+
+        //on met les hashmap dans des arralist pour reverse facilement
+
+        int zero = 0;
+        List<String> queryListTriee1 = new ArrayList<String>();
+        for (Map.Entry<String, Double> entry : mapTriee1.entrySet()) {
+            queryListTriee1.add(entry.getKey());
+            if(entry.getValue().equals(0.0)){
+                zero++;
+            }
+        }
+
+        int un = zero;
+
+        System.out.println(queryListTriee1);
+        Collections.reverse(queryListTriee1);
+        System.out.println(queryListTriee1);
+        System.out.println("Il y a "+zero+" zeros dans cette liste");
+
+        zero = 0;
+
+        List<String> queryListTriee2 = new ArrayList<String>();
+        for (Map.Entry<String, Double> entry : mapTriee2.entrySet()) {
+            queryListTriee2.add(entry.getKey());
+            if(entry.getValue().equals(0.0)){
+                zero++;
+            }
+        }
+
+
+        int deux = zero;
+
+
+        System.out.println(queryListTriee2);
+        Collections.reverse(queryListTriee2);
+        System.out.println(queryListTriee2);
+        System.out.println("Il y a "+zero+" zeros dans cette liste");
+
+        zero = 0;
+
+        List<String> queryListTriee3 = new ArrayList<String>();
+        for (Map.Entry<String, Double> entry : mapTriee3.entrySet()) {
+            queryListTriee3.add(entry.getKey());
+            if(entry.getValue().equals(0.0)){
+                zero++;
+            }
+        }
+
+
+        int trois = zero;
+
+
+
+        System.out.println(queryListTriee3);
+        Collections.reverse(queryListTriee3);
+        System.out.println(queryListTriee3);
+        System.out.println("Il y a "+zero+" zeros dans cette liste");
+
+
+
+        zero = 0;
+
+        List<String> queryListTriee4 = new ArrayList<String>();
+        for (Map.Entry<String, Double> entry : mapTriee4.entrySet()) {
+            queryListTriee4.add(entry.getKey());
+            if(entry.getValue().equals(0.0)){
+                zero++;
+            }
+
+        }
+
+
+
+        int quatre = zero;
+
+        System.out.println(queryListTriee4);
+        Collections.reverse(queryListTriee4);
+        System.out.println(queryListTriee4);
+        System.out.println("Il y a "+zero+" zeros dans cette liste");
+
+
+
+
+        zero = 0;
+
+        List<String> queryListTriee5 = new ArrayList<String>();
+        for (Map.Entry<String, Double> entry : mapTriee5.entrySet()) {
+            queryListTriee5.add(entry.getKey());
+            if(entry.getValue().equals(0.0)){
+                zero++;
+            }
+        }
+
+        int cinq=zero;
+
+        System.out.println(queryListTriee5);
+        Collections.reverse(queryListTriee5);
+        System.out.println(queryListTriee5);
+        System.out.println("Il y a "+zero+" zeros dans cette liste");
+
+
+
+        zero = 0;
+
+        List<String> queryListTriee6 = new ArrayList<String>();
+        for (Map.Entry<String, Double> entry : mapTriee6.entrySet()) {
+            queryListTriee6.add(entry.getKey());
+            if(entry.getValue().equals(0.0)){
+                zero++;
+            }
+        }
+
+
+        int six = zero;
+
+        System.out.println(queryListTriee6);
+        Collections.reverse(queryListTriee6);
+        System.out.println(queryListTriee6);
+        System.out.println("Il y a "+zero+" zeros dans cette liste");
+
+
+
+        zero = 0;
+
+        List<String> queryListTriee7 = new ArrayList<String>();
+        for (Map.Entry<String, Double> entry : mapTriee7.entrySet()) {
+            queryListTriee7.add(entry.getKey());
+            if(entry.getValue().equals(0.0)){
+                zero++;
+            }
+        }
+
+
+        int sept = zero;
+
+
+        //System.out.println(queryListTriee7);
+        Collections.reverse(queryListTriee7);
+        //System.out.println(queryListTriee7);
+        //System.out.println("Il y a "+zero+" zeros dans cette liste");
+
+
+       // System.out.println(" "+un+" - "+ deux+"  -  "+trois+"  - "+quatre+" - "+cinq+" - "+six+" - "+ sept);
+
+        PrintWriter testOut = null;
+        PrintWriter run2 = null;
+        PrintWriter run3 = null;
+        PrintWriter run4 = null;
+        PrintWriter run5 = null;
+
+
+
+        try{
+            testOut = new PrintWriter(new OutputStreamWriter(
+                    new BufferedOutputStream(new FileOutputStream("MadjidPierreBunyaminFrançois_03_01_LTN_articles.txt"))));
+            run2 = new PrintWriter(new OutputStreamWriter(
+                    new BufferedOutputStream(new FileOutputStream("MadjidPierreBunyaminFrançois_03_02_LTN_articles.txt"))));
+            run3 = new PrintWriter(new OutputStreamWriter(
+                    new BufferedOutputStream(new FileOutputStream("MadjidPierreBunyaminFrançois_03_03_LTN_articles.txt"))));
+            run4 = new PrintWriter(new OutputStreamWriter(
+                    new BufferedOutputStream(new FileOutputStream("MadjidPierreBunyaminFrançois_03_04_LTN_articles.txt"))));
+            run5 = new PrintWriter(new OutputStreamWriter(
+                    new BufferedOutputStream(new FileOutputStream("MadjidPierreBunyaminFrançois_03_05_LTN_articles.txt"))));
+        } catch (Exception e1) {
+        e1.printStackTrace();
+        System.out.println(e1);
+        }
+
+        for(int run=0; run<5; run++){
+            switch (run){
+                case 1:
+                for(int compt=0;compt<1500;compt++){
+                    testOut.println(queryListTriee1.get(compt).replaceAll("<>", String.valueOf(compt+1)));
+                }
+                for(int compt=0;compt<1500;compt++){
+                     testOut.println(queryListTriee2.get(compt).replaceAll("<>", String.valueOf(compt+1)));
+                }
+                for(int compt=0;compt<1500;compt++){
+                    testOut.println(queryListTriee3.get(compt).replaceAll("<>", String.valueOf(compt+1)));
+                }
+                for(int compt=0;compt<1500;compt++){
+                    testOut.println(queryListTriee4.get(compt).replaceAll("<>", String.valueOf(compt+1)));
+                }
+                for(int compt=0;compt<1500;compt++){
+                    testOut.println(queryListTriee5.get(compt).replaceAll("<>", String.valueOf(compt+1)));
+                }
+                for(int compt=0;compt<1500;compt++){
+                    testOut.println(queryListTriee6.get(compt).replaceAll("<>", String.valueOf(compt+1)));
+                }
+                for(int compt=0;compt<1500;compt++){
+                    testOut.println(queryListTriee7.get(compt).replaceAll("<>", String.valueOf(compt+1)));
+                }
+                break;
+                case 2:
+                    for(int compt=1500;compt<3000;compt++){
+                        run2.println(queryListTriee1.get(compt).replaceAll("<>", String.valueOf(compt+1-1500)));
+                    }
+                    for(int compt=1500;compt<3000;compt++){
+                        run2.println(queryListTriee2.get(compt).replaceAll("<>", String.valueOf(compt+1-1500)));
+                    }
+                    for(int compt=1500;compt<3000;compt++){
+                        run2.println(queryListTriee3.get(compt).replaceAll("<>", String.valueOf(compt+1-1500)));
+                    }
+                    for(int compt=1500;compt<3000;compt++){
+                        run2.println(queryListTriee4.get(compt).replaceAll("<>", String.valueOf(compt+1-1500)));
+                    }
+                    for(int compt=1500;compt<3000;compt++){
+                        run2.println(queryListTriee5.get(compt).replaceAll("<>", String.valueOf(compt+1-1500)));
+                    }
+                    for(int compt=1500;compt<3000;compt++){
+                        run2.println(queryListTriee6.get(compt).replaceAll("<>", String.valueOf(compt+1-1500)));
+                    }
+                    for(int compt=1500;compt<3000;compt++){
+                        run2.println(queryListTriee7.get(compt).replaceAll("<>", String.valueOf(compt+1-1500)));
+                    }
+                    break;
+
+
+                case 3:
+                    for(int compt=3000;compt<4500;compt++){
+                        run3.println(queryListTriee1.get(compt).replaceAll("<>", String.valueOf(compt+1-3000)));
+                    }
+                    for(int compt=3000;compt<4500;compt++){
+                        run3.println(queryListTriee2.get(compt).replaceAll("<>", String.valueOf(compt+1-3000)));
+                    }
+                    for(int compt=3000;compt<4500;compt++){
+                        run3.println(queryListTriee3.get(compt).replaceAll("<>", String.valueOf(compt+1-3000)));
+                    }
+                    for(int compt=3000;compt<4500;compt++){
+                        run3.println(queryListTriee4.get(compt).replaceAll("<>", String.valueOf(compt+1-3000)));
+                    }
+                    for(int compt=3000;compt<4500;compt++){
+                        run3.println(queryListTriee5.get(compt).replaceAll("<>", String.valueOf(compt+1-3000)));
+                    }
+                    for(int compt=3000;compt<4500;compt++){
+                        run3.println(queryListTriee6.get(compt).replaceAll("<>", String.valueOf(compt+1-3000)));
+                    }
+                    for(int compt=3000;compt<4500;compt++){
+                        run3.println(queryListTriee7.get(compt).replaceAll("<>", String.valueOf(compt+1-3000)));
+                    }
+                    break;
+
+
+
+
+                case 4:
+                    for(int compt=4500;compt<6000;compt++){
+                        run4.println(queryListTriee1.get(compt).replaceAll("<>", String.valueOf(compt+1-4500)));
+                    }
+                    for(int compt=4500;compt<6000;compt++){
+                        run4.println(queryListTriee2.get(compt).replaceAll("<>", String.valueOf(compt+1-4500)));
+                    }
+                    for(int compt=4500;compt<6000;compt++){
+                        run4.println(queryListTriee3.get(compt).replaceAll("<>", String.valueOf(compt+1-4500)));
+                    }
+                    for(int compt=4500;compt<6000;compt++){
+                        run4.println(queryListTriee4.get(compt).replaceAll("<>", String.valueOf(compt+1-4500)));
+                    }
+                    for(int compt=4500;compt<6000;compt++){
+                        run4.println(queryListTriee5.get(compt).replaceAll("<>", String.valueOf(compt+1-4500)));
+                    }
+                    for(int compt=4500;compt<6000;compt++){
+                        run4.println(queryListTriee6.get(compt).replaceAll("<>", String.valueOf(compt+1-4500)));
+                    }
+                    for(int compt=4500;compt<6000;compt++){
+                        run4.println(queryListTriee7.get(compt).replaceAll("<>", String.valueOf(compt+1-4500)));
+                    }
+                    break;
+
+
+            }
+
+        }
+
+        testOut.close();
+        run2.close();
+        run3.close();
+        run4.close();
+        run5.close();
+
+
+
+
         PrintWriter out = null;
         PrintWriter out2 = null;
         PrintWriter out3 = null;
@@ -126,8 +500,6 @@ public class Indexation {
 
 
         try {
-
-
             out = new PrintWriter(new OutputStreamWriter(
                        new BufferedOutputStream(new FileOutputStream("MadjidPierreBunyaminFrançois_01_01_LTN_articles.txt"))));
 
@@ -142,23 +514,6 @@ public class Indexation {
 
             out5 = new PrintWriter(new OutputStreamWriter(
                     new BufferedOutputStream(new FileOutputStream("MadjidPierreBunyaminFrançois_01_05_LTN_articles.txt"))));
-
-            /*
-            FileWriter runs5 = new FileWriter("MadjidPierreBunyaminFrançois_01_05_LTN_articles.txt");
-            BufferedWriter bw5 = new BufferedWriter(runs5);
-
-            FileWriter runs4 = new FileWriter("MadjidPierreBunyaminFrançois_01_04_LTN_articles.txt");
-            BufferedWriter bw4 = new BufferedWriter(runs4);
-
-            FileWriter runs3 = new FileWriter("MadjidPierreBunyaminFrançois_01_03_LTN_articles.txt");
-            BufferedWriter bw3 = new BufferedWriter(runs3);
-
-            FileWriter runs2 = new FileWriter("MadjidPierreBunyaminFrançois_01_02_LTN_articles.txt");
-            BufferedWriter bw2 = new BufferedWriter(runs2);
-
-            FileWriter runs1 = new FileWriter("MadjidPierreBunyaminFrançois_01_01_LTN_articles.txt");
-            BufferedWriter bw1 = new BufferedWriter(runs1);
-            */
 
         System.out.println("Apres le tri");
         compteur = 0;
@@ -196,6 +551,7 @@ public class Indexation {
             compteur++;
         }
 
+
         out.close();
             out2.close();
             out3.close();
@@ -206,44 +562,8 @@ public class Indexation {
             System.out.println(e1);
         }
 
-        /*
-
-        68000
-
-        66500
-
-        65000
-
-
-
-         */
-
         TreeMap<String, Double> treeMap = new TreeMap<>(Collections.reverseOrder());
         treeMap.putAll(mapTriee);
 
-
-
-        /*
-        System.out.println("renversement");
-        compteur=0;
-        Iterator it = MapUtil.valueIterator(treeMap);
-        while(it.hasNext()) {
-            System.out.println(it.next());
-            compteur++;
-        }
-*/
-
-
-        /*
-
-        Il faut donc ordonner toutes ces ligne selon ordre decroissant du score de cos similarite
-        et mettre dans 5 fichier different (1500 lignes par fichiers)
-
-        a confirmer je sais pas si c'est juste
-
-        cordialement
-
-
-         */
     }
 }
